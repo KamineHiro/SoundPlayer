fa6fbdc6151eb846be1eb6afd0774422f275c484
com.hirokiakamine <hiroki080602@icloud.com>
2025-01-04 18:59:36 +0900

    音声追加

--- src/constants/sounds.ts
+++ src/constants/sounds.ts
@@ -9,11 +9,23 @@ export const SOUNDS: Sound[] = [
     url: 'https://soundeffect-lab.info/sound/voice/mp3/line-girl1/line-girl1-hazimemashite1.mp3'
   },
   {
-    label: 'a',
+    label: 'a1',
     url: 'https://soundeffect-lab.info/sound/voice/mp3/line-girl1/line-girl1-akemashiteomedetougozaimasu1.mp3'
   },
   {
     label: 'b',
     url: 'https://soundeffect-lab.info/sound/voice/mp3/line-girl1/line-girl1-byebye1.mp3'
+  },
+  {
+    label: 'g',
+    url: 'https://soundeffect-lab.info/sound/voice/mp3/line-girl1/line-girl1-gogogogomennasai1.mp3'
+  },
+  {
+    label: 'a2',
+    url: 'https://soundeffect-lab.info/sound/voice/mp3/line-girl1/line-girl1-arigatougozaimasu1.mp3'
+  },
+  {
+    label: 'n',
+    url: 'https://soundeffect-lab.info/sound/voice/mp3/line-girl1/line-girl1-nandeyanen1.mp3'
   }
 ];
